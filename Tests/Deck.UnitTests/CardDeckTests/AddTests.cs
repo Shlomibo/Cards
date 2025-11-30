c335d69f0935a040081cb85854776250aa54bb5b
Shlomi Borovitz <shlomi.newmail@gmail.com>
2025-11-30 23:52:00 +0200

    Add tests

---
+++ Tests/Deck.UnitTests/CardDeckTests/AddTests.cs
@@ -0,0 +1,44 @@
+using System;
+
+using AwesomeAssertions;
+
+namespace Deck.UnitTests.CardDeckTests;
+
+public class AddTests : CardDeckTestsBase
+{
+    [Test]
+    public void WhenAddingToAnEmptyList()
+    {
+        using var x = CreateTestData();
+        RandomCard newCard = new();
+
+        x.TestSubject.Add(newCard);
+
+        x.TestSubject.Should().ContainSingle()
+            .Which.Should().Be(newCard);
+    }
+
+    [Test]
+    public void WhenAddingToAListWithAnItem()
+    {
+        RandomCard[] cards = [.. CreateCards(1)];
+        using var x = CreateTestData(cards.Reverse());
+        RandomCard newCard = new();
+
+        x.TestSubject.Add(newCard);
+
+        x.TestSubject.Should().BeEquivalentTo([.. cards, newCard]);
+    }
+
+    [Test]
+    public void WhenAddingToAListWithManyItems()
+    {
+        RandomCard[] cards = [.. CreateCards(10)];
+        using var x = CreateTestData(cards.Reverse());
+        RandomCard newCard = new();
+
+        x.TestSubject.Add(newCard);
+
+        x.TestSubject.Should().BeEquivalentTo([.. cards, newCard]);
+    }
+}
